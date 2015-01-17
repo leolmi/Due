44eb816bd8d9ca5b70928eaebaca3be8102fd2df
leolmi <leo.olmi@gmail.com>
2015-01-17 21:55:38 +0100

    ottimizzazioni

--- client/components/socket/socket.service.js
+++ client/components/socket/socket.service.js
@@ -5,12 +5,13 @@ angular.module('dueAppApp')
   .factory('socket', function(socketFactory) {
 
     // socket.io now auto-configures its connection when we ommit a connection url
-    var ioSocket = io('', {
-      // Send auth token on connection, you will need to DI the Auth service above
-      // 'query': 'token=' + Auth.getToken()
-      //path: '/socket.io-client'
-      path: '/'
-    });
+    //var ioSocket = io('', {
+    //  // Send auth token on connection, you will need to DI the Auth service above
+    //  // 'query': 'token=' + Auth.getToken()
+    //  path: '/socket.io-client'
+    //});
+
+    var ioSocket = io('https://nameless-everglades-6768.herokuapp.com:8000');
 
     var socket = socketFactory({
       ioSocket: ioSocket
